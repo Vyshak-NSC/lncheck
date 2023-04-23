a12e5206390fbdd50e874cb701fe73cc3b771b61
Vyshak <vyshaknsc02@gmail.com>
2023-04-23 21:32:23 +0530

    Initial commit

--- test3.js
+++ test3.js
@@ -2,17 +2,23 @@ import fetch from "node-fetch";
 import cheerio from "cheerio";
 
 const url = "https://readnovelfull.me/dimensional-descent/";
+let output = document.getElementsById('out');
 
-fetch(url)
-  .then((response) => response.text())
-  .then((html) => {
-	      const $ = cheerio.load(html);
-	      const latestChapter = $("div.item-value a[href^='/dimensional-descent/chapter']")
-	        .first()
-	        .attr("title");
-	      console.log(`Latest chapter: ${latestChapter}`);
-	    })
-  .catch((error) => {
-	      console.log(`Error fetching ${url}: ${error}`);
-	    });
+function get_chap() {
+	fetch(url)
+		.then((response) => response.text())
+		.then((html) => {
+			const $ = cheerio.load(html);
+			const latestChapter = $("div.item-value a[href^='/dimensional-descent/chapter']")
+				.first()
+				.attr("title");
+			alert(latestChapter); //console.log(`Latest chapter: ${latestChapter}`);
+		})
+		.catch((error) => {
+			console.log(`Error fetching ${url}: ${error}`);
+		});
 
+}
+
+const btn = document.getElementsById("go");
+btn.addEventListener('click', get_chap)
